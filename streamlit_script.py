6002a29c4037cbdf23372ccaf0f7813010e7ae63
SorayaG3 <gutter@hotitem.nl>
2025-04-08 17:37:26 +0000

    titel

--- streamlit_script.py
+++ streamlit_script.py
@@ -1,12 +1,3 @@
 import streamlit as st
-import pandas as pd
-# import plotly.express as px
-# import plotly.graph_objects as go
-
-# ======= Header toevoegen ========
-st.title("Dashboard Schoenverkoop")
-
-# ======= Dataset inladen ========
-df = pd.read_csv("exclusieve_schoenen_verkoop_met_locatie.csv")
 
 st.write("DOE HET GEWOON")
